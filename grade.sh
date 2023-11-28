c51d4ecf344b9dc7367715e4a176cc145d51260a
bnghiem18 <118479808+bnghiem18@users.noreply.github.com>
2023-11-28 10:47:09 -0800

    grade.sh

--- grade.sh
+++ grade.sh
@@ -14,3 +14,45 @@ echo 'Finished cloning'
 
 # Then, add here code to compile and run, and do any post-processing of the
 # tests
+cp -r lib *.java grading-area
+
+#checking for correct file
+if [[ -f 'student-submission/ListExamples.java' ]] 
+    then 
+        cp -r student-submission/ListExamples.java grading-area
+    else 
+        echo "File is invalid"
+        exit
+    fi
+echo "Exited loop"
+
+cd grading-area
+
+
+javac -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar *.java > CompileOutput.txt
+
+#checking for compile error 
+if [[ 'find grading-area/CompileOutput.txt -empty' ]]
+    then
+        echo "Compiled Successfully!"
+    else
+        #if failed, print out what the compile error was
+        cat grading-area/CompileOutput.txt
+        exit
+    fi
+
+java -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples > JUnitOutput.txt
+
+grep -A 1 "FAILURES!!!" JUnitOutput.txt > GrepCheck.txt
+
+#checking if there are failures in the JUNIT
+if grep -q "FAILURES!!!" JUnitOutput.txt 
+    then
+        echo "There are failures"
+        result = "`grep "Tests run:" JUnitOutput.txt`"
+        echo "$result"
+    else
+        echo "Passed all Tests"
+        exit
+    fi
+
